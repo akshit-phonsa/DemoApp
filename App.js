8efc0d51abc3f26e7030955c1d296446a24616c3
akshit-phonsa <phonsaakshit@gmail.com>
2023-07-21 17:15:58 +0530

    Final TODO LIST

--- App.js
+++ App.js
@@ -1,43 +1,210 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
-// Firestore Imports Start
-// import firestore from '@react-native-firebase/firestore';
-// Firestore Imports Stop
-
-// RealTime Imports Start
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  StatusBar,
+  FlatList,
+  Alert,
+} from 'react-native';
 import database from '@react-native-firebase/database';
 
-// RealTime Imports Stop
 export default function App() {
+  const [inputTextValue, setInputTextValue] = useState(null);
+  const [list, setList] = useState(null);
+  const [isUpdateData, setIsUpdateData] = useState(false);
+  const [selectedCardIndex, setSelectedCardIndex] = useState(null);
+
   useEffect(() => {
     getDatabase();
   }, []);
 
-  const [myData, setMyData] = useState(null);
-
-  const getDatabase = async()=>{
+  const getDatabase = async () => {
     try {
-      // FireStore Start
-      // const data = await firestore().collection("testing").doc("g2kcQX8PXQj9x3xxVJu3").get();
-
-      // setMyData(data._data);
-      // // console.log(data._data);
-      // FireStore End
-
-      // Realtime Start
-        const data = await database().ref("users/1").once("value");
-        console.log(data);
-        setMyData(data.val());
-      // Realtime End
+      // const data = await database().ref('todo').once('value');
+      const data = await database()
+        .ref('todo')
+        .on('value', tempData => {
+          console.log(data);
+          setList(tempData.val());
+        });
     } catch (err) {
       console.log(err);
     }
-  }
-  
+  };
+
+  const handleAddData = async () => {
+    try {
+      if (inputTextValue.length > 0) {
+        const index = list === null ? 0 : list.length;
+        
+        const response = await database().ref(`todo/${index}`).set({
+          value: inputTextValue,
+        });
+
+        console.log(response);
+
+        setInputTextValue('');
+      } else {
+        alert('Please Enter Value & Then Try Again');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateData = async () => {
+    try {
+      if (inputTextValue.length > 0) {
+        const response = await database()
+          .ref(`todo/${selectedCardIndex}`)
+          .update({
+            value: inputTextValue,
+          });
+
+        console.log(response);
+        setInputTextValue('');
+        setIsUpdateData(false);
+      } else {
+        alert('Please Enter Value & Then Try Again');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleCardPress = (cardIndex, cardValue) => {
+    try {
+      setIsUpdateData(true);
+      setSelectedCardIndex(cardIndex);
+      setInputTextValue(cardValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleCardLongPress = (cardIndex, cardValue) => {
+    try {
+      Alert.alert('Alert', `Are You Sure To Delete ${cardValue} ?`, [
+        {
+          text: 'Cancel',
+          onPress: () => {
+            console.log('Cancel Is Press');
+          },
+        },
+        {
+          text: 'Ok',
+          onPress: async () => {
+            try {
+              const response = await database()
+                .ref(`todo/${cardIndex}`)
+                .remove();
+
+              setInputTextValue('');
+              setIsUpdateData(false);
+              console.log(response);
+            } catch (err) {
+              console.log(err);
+            }
+          },
+        },
+      ]);
+
+      // setIsUpdateData(true);
+      // setSelectedCardIndex(cardIndex);
+      // setInputTextValue(cardValue);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <View>
-      <Text>Name: {myData ? myData.name : `Loading...`}</Text>
-      <Text>Age: {myData ? myData.age : `Loading...`}</Text>
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <View>
+        <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>
+          Todo App
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter Any Value"
+          value={inputTextValue}
+          onChangeText={value => setInputTextValue(value)}
+        />
+        {!isUpdateData ? (
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => handleAddData()}>
+            <Text style={{color: '#fff'}}>Add</Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => handleUpdateData()}>
+            <Text style={{color: '#fff'}}>Update</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <View style={styles.cardContainer}>
+        <Text style={{marginVertical: 20, fontSize: 20, fontWeight: 'bold'}}>
+          Todo List
+        </Text>
+
+        <FlatList
+          data={list}
+          renderItem={item => {
+            const cardIndex = item.index;
+            if (item.item !== null) {
+              return (
+                <TouchableOpacity
+                  style={styles.card}
+                  onPress={() => handleCardPress(cardIndex, item.item.value)}
+                  onLongPress={() =>
+                    handleCardLongPress(cardIndex, item.item.value)
+                  }>
+                  <Text>{item.item.value}</Text>
+                </TouchableOpacity>
+              );
+            }
+          }}
+        />
+      </View>
     </View>
-  )
+  );
 }
+
+const {height, width} = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  inputBox: {
+    width: width - 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    marginVertical: 10,
+    padding: 10,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 50,
+  },
+  cardContainer: {
+    marginVertical: 20,
+  },
+  card: {
+    backgroundColor: '#fff',
+    width: width - 40,
+    padding: 20,
+    borderRadius: 30,
+    marginVertical: 10,
+  },
+});
